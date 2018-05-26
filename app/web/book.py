065fb7013bb18b7192e9a19b467a37ccef767e75
KeithTt <shengyongp@oupeng.com>
2018-05-26 22:01:29 +0800

    update book_detail

--- app/web/book.py
+++ app/web/book.py
@@ -1,20 +1,23 @@
 
 from flask import jsonify, request, render_template, flash
+from flask_login import current_user
+
 from app.forms.book import SearchForm
 from app.models.gift import Gift
 from app.models.wish import Wish
+from app.view_models.trade import TradeInfo
 from app.web import web
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
 from app.view_models.book import BookViewModel, BookCollection
-import json
+# import json
 
 # 使用蓝图注册视图函数
 @web.route('/book/search/')
 def search():
-    '''
+    """
     http://localhost:8088/book/search/?q=红楼梦&page=1
-    '''
+    """
     form = SearchForm(request.args)
     books = BookCollection()
 
@@ -51,6 +54,7 @@ def search():
         flash('搜索的关键字不符合要求，请重新输入关键字')
     return render_template('search_result.html', books=books)
 
+
 @web.route('/book/<isbn>/detail')
 def book_detail(isbn):
     has_in_gifts = False
@@ -61,25 +65,39 @@ def book_detail(isbn):
     yushu_book.search_by_isbn(isbn)
     book = BookViewModel(yushu_book.first)
 
+    # 判断用户是否登录
+    if current_user.is_authenticated:
+        if Gift.query.filter_by(uid=current_user.id, isbn=isbn, launched=False).first():
+            has_in_gifts = True
+        if Wish.query.filter_by(uid=current_user.id, isbn=isbn, launched=False).first():
+            has_in_wishes = True
+
     # 查询出所有赠送者的信息
     trade_gifts = Gift.query.filter_by(isbn=isbn, launched=False).all()
     # 查询出所有索要者的信息
     trade_wishes = Wish.query.filter_by(isbn=isbn, launched=False).all()
 
-    return render_template('book_detail.html', book=book, wishes=[], gifts=[])
+    trade_gifts_model = TradeInfo(trade_gifts)
+    trade_wishes_model = TradeInfo(trade_wishes)
 
-@web.route('/test')
-def test():
-    '''
-    http://localhost:8088/test
-    '''
-    r = {
-        'name': '',
-        'age': 18
-    }
-    # flash可以多次调用，消息闪现的将是一个列表
-    flash('Hello KeithTt', category='error')
-    flash('Hello Jerry', category='warning')
-    # 模板 HTML
-    # return jsonify(r)
-    return render_template('test4.html', data=r,)
+    return render_template('book_detail.html',
+                           book=book,
+                           wishes=trade_wishes_model, gifts=trade_gifts_model,
+                           has_in_gifts=has_in_gifts, has_in_wishes=has_in_wishes)
+
+
+# @web.route('/test')
+# def test():
+#     """
+#     http://localhost:8088/test
+#     """
+#     r = {
+#         'name': '',
+#         'age': 18
+#     }
+#     # flash可以多次调用，消息闪现的将是一个列表
+#     flash('Hello KeithTt', category='error')
+#     flash('Hello Jerry', category='warning')
+#     # 模板 HTML
+#     # return jsonify(r)
+#     return render_template('test4.html', data=r,)
