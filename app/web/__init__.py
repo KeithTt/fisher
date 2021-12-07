17ecccd0d3988d5e922db54987c2031aa275ecf6
KeithTt <329640305@qq.com>
2021-12-07 14:22:40 +0800

    add exception in app_errorhandler

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, render_template
 
 # 蓝图 blueprint，蓝图是不能独立存在的，必须要插入flask的核心对象app里，每一个蓝图又能够插入很多视图函数
-# 原本是把视图函数直接注册到app核心对象
 
 web = Blueprint(name='web', import_name=__name__)  # 创建一个蓝图
 
 
 @web.app_errorhandler(404)
-def not_found(e):
+def page_not_found(e):
     """监控所有404异常，并返回指定页面"""
     return render_template('404.html'), 404
+
+
+@web.app_errorhandler(500)
+def internal_server_error(e):
+    """监控所有500异常，并返回指定页面"""
+    return render_template('500.html'), 500
 
 
 from app.web import book
